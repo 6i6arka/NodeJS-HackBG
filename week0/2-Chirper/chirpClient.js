64f59020adced9af3513f8f93ea92204a5decba2
6i6arka <rado.milushev@gmail.com>
2014-10-14 13:42:30 +0300

    Repo fix and Chirpr almost done

--- week0/2-Chirper/chirpClient.js
+++ week0/2-Chirper/chirpClient.js
@@ -1,48 +1,89 @@
 var http = require('http'),
-    querystring = require('querystring'),
+    fs = require('fs'),
     ArgumentParser = require("argparse").ArgumentParser,
     parser = new ArgumentParser(),
+    config = require('./config.json'),
     actions = {
         "getall": getAllChirps,
+        "getusers": getAllUsers,
         "register": registerUser,
         "create": createChirp
-    }, post_options, post_req;
+    };
 
 parser.addArgument(["--getall"], {action: 'storeTrue'});
-parser.addArgument(["--create"]);
+parser.addArgument(["--getusers"], {action: 'storeTrue'});
+parser.addArgument(["--create"], {action: 'storeTrue'});
+parser.addArgument(["--message"]);
+parser.addArgument(["--register"], {action: 'storeTrue'});
+parser.addArgument(["--user"]);
 var args = parser.parseArgs();
 
 Object.keys(args).forEach(function(arg){
-    if(args[arg]){
+    if(actions[arg] && args[arg]){
         actions[arg].call(null, args[arg]);
     }
 });
 
 function getAllChirps() {
-    http.get("http://localhost:8080/all_chirps", function(res){
-        res.setEncoding('utf8');
-        res.on('data', function(data){
-            console.log(data.toString());
-        });
+    getRequest("/all_chirps", function(data){
+        console.log(data);
+    });
+}
+
+function getAllUsers() {
+    getRequest("/all_users", function(data){
+        console.log(data);
     });
 }
 
 function registerUser(){
+    if (args.user) {
+        postRequest({
+            path: "/register",
+            postData: {
+                userName: args.user
+            },
+            action: function(data){
+                var parsedData = JSON.parse(data);
+                console.log(parsedData);
+                config.key = parsedData.key;
+                config.user = parsedData.user;
+                fs.writeFile("./config.json", JSON.stringify(config, null, "\t"));
+            }
+        });
+    }
+}
 
+function createChirp() {
+    postRequest({
+        path: "/chirp",
+        postData: {
+            chirp: args.message || "Hello world!",
+            userName: config.user
+        },
+        action: function(data){
+            console.log(data);
+        }
+    });
 }
 
 function postRequest(opt){
     var post_options = {
-        host: "localhost",
-        port: "8080",
+        host: config.apiUrl.split(":")[0],
+        port: config.apiUrl.split(":")[1],
         path: opt.path,
         method: "POST"
-    };
+        },
+        payload = '';
 
     var post_req = http.request(post_options, function(res){
         res.setEncoding('utf8');
         res.on('data', function(data){
-            console.log(data);
+            payload += data;
+        });
+
+        res.on('end', function(){
+            opt.action(payload);
         });
     });
 
@@ -51,12 +92,12 @@ function postRequest(opt){
     post_req.end();
 }
 
-function createChirp(chirp/*string*/) {
-    postRequest({
-        path: "/chirp",
-        postData: {
-            chirp: chirp,
-            user: currentUser
-        }
+function getRequest(path, action) {
+    http.get("http://" + config.apiUrl + path, function(res){
+        res.setEncoding('utf8');
+        res.on('data', function(data){
+            action(data);
+        });
     });
 }
+
--- week0/2-Chirper/chirpServer.js
+++ week0/2-Chirper/chirpServer.js
@@ -1,7 +1,7 @@
 var http = require('http'),
     fs = require('fs'),
-    chirpsFile = require('./chirps.json');
-//    usersFile = require('./config.json');
+    chirps = require('./chirps.json'),
+    users = require('./users.json');
 
 http.createServer(function(req, res){
     var payload = "",
@@ -10,28 +10,66 @@ http.createServer(function(req, res){
             "/chirp": createChirp,
             "/all_users": sendUsers,
             "/register": createUser,
-            "/my_chirps": returnMyChirps,
+            "/my_chirps": returnUserChirps,
             "/chirps": sendChirps
         };
 
     function sendAllChirps(){
         res.writeHead(200, "OK", {'Content-Type':'application/json'});
-        res.end(JSON.stringify(chirpsFile));
+        res.end(JSON.stringify(chirps, null, '\t'));
     }
 
     function createChirp(){
-        console.log(JSON.parse(payload));
+        var chirpsLen = chirps.length,
+            chirpingUser = JSON.parse(payload);
+        users.forEach(function(item){
+            if(item.user === chirpingUser.userName){
+                chirps.push({
+                    userId: item.userId,
+                    chirpId: chirps.length,
+                    chirpText: chirpingUser.chirp,
+                    chirpTime: getFormattedDate()
+                });
+                item.chirps += 1;
+                fs.writeFile('./chirps.json', JSON.stringify(chirps, null, "\t"));
+                fs.writeFile('./users.json', JSON.stringify(users, null, "\t"));
+            }
+        });
+        res.end(chirpsLen !== chirps.length ? "Success" : "No such user");
     }
 
     function sendUsers() {
-
+        res.writeHead(200, "OK", {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(users, null, '  '));
     }
 
-    function createUser(user) {
-        console.log(payload);
+    function getFormattedDate() {
+        var d = new Date();
+        return d.getDay() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear() + ' ' + d.getHours() + ":" + d.getMinutes();
     }
 
-    function returnMyChirps(opt) {
+    function createUser() {
+        console.log(typeof payload);
+        var newUser = JSON.parse(payload),
+            response = {
+                "user": newUser.userName,
+                "userId": users.length,
+                "chirps": [],
+                "key": require('randomstring').generate(8)
+            };
+        users.filter(function(item){
+            return item.user === newUser.userName;
+        }).length === 0 ? users.push(response) : (response = "User name taken!");
+
+        fs.writeFile("./users.json", JSON.stringify(users, null, "\t"), null, function(err){
+            console.log(err);
+        });
+
+        res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(response));
+    }
+
+    function returnUserChirps(opt) {
 
     }
 
@@ -51,5 +89,5 @@ http.createServer(function(req, res){
             routes[req.url].call(null, req, res);
         }
         res.end();
-    })
+    });
 }).listen(8080);
